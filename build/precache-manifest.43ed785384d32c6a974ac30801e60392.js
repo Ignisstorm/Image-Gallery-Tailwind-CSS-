daf08a2877b771e90fab038fcc9727ff76c8cd4b
Erika Loots <43708340+Ignisstorm@users.noreply.github.com>
2020-06-15 11:04:19 +0200

    Add files via upload

---
+++ build/precache-manifest.43ed785384d32c6a974ac30801e60392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03febdda2f5f829094dc21e1d074890e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d3bd1b9795a067b9d0",
+    "url": "/static/css/main.cda36a3e.chunk.css"
+  },
+  {
+    "revision": "f7f70d0a001e97222986",
+    "url": "/static/js/2.999a43d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.999a43d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d3bd1b9795a067b9d0",
+    "url": "/static/js/main.7c37e295.chunk.js"
+  },
+  {
+    "revision": "3ec2a9e7fd8b520b8377",
+    "url": "/static/js/runtime-main.3d121836.js"
+  }
+]);
